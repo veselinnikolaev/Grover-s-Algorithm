ab9b7c51c6be4a6c3f32c5cbd574fdd9ca4f8fa7
veselinnikolaev <vslnnikolaev@gmail.com>
2026-07-08 18:09:18 +0200

    Remove debug logging for circuit properties in `tn_metrics.py`

--- tn_metrics.py
+++ tn_metrics.py
@@ -70,7 +70,6 @@ def rehearse_amplitude(qc, target_bitstring, optimize=None, verbose=True,
         optimize = _make_optimizer(max_time=max_time, max_repeats=max_repeats)
 
     circ = quimb_circuit(qc)
-    print(f"    [debug] circ.N={circ.N}, circ.num_gates={circ.num_gates}, id(circ)={id(circ)}")
 
     t0 = time.perf_counter()
 
